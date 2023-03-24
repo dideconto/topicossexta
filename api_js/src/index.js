06f38c1f7fb57c4771041084ccd2ab0ade8e0d63
Diogo Deconto <dideconto@gmail.com>
2023-03-24 11:36:24 -0300

    API JS - Pegar parâmentro da URL

--- api_js/src/index.js
+++ api_js/src/index.js
@@ -6,15 +6,18 @@ const app = express();
 //Configurando uma requisição com o método GET para 
 //a raiz da aplicação 
 app.get("/", function(request, response){
-    console.log("Primeira requisição recebida!");
     response.status(200).json({ message : "Reposta enviada em JSON"});
 });
 
 //EXERCÍCIO - Criar uma nova rota/funcionalidade para receber uma 
-//informação pelo corpo da requisição
-
-//EXERCÍCIO - Criar uma nova rota/funcionalidade para receber uma 
 //informação pela URL da requição
+app.get("/:categoria", (request, response) => {
+    console.log(request.params.categoria);
+});
+
+//EXERCÍCIO - Criar uma nova rota/funcionalidade com método POST 
+//para receber uma informação pelo corpo da requisição
+//POSTMAN, INSOMNIA e RESTCLIENT
 
 //O comando listen roda a aplicação
 app.listen(3000, function(){
