a9a9bdba6e95159015930e090337ddfd383d0a79
Diogo Deconto <dideconto@gmail.com>
2023-05-12 11:57:00 -0300

    FRONT AMBIENTACAO - Projeto Base

---
+++ ambientacao-react/src/components/soma.tsx
@@ -0,0 +1,10 @@
+function Soma(){
+    return (<h2>Função de soma</h2>);
+}
+
+// Criar duas caixas de texto
+// Criar um botão
+// Somar os dois valores
+// Exibir em alert e depois no HTML
+
+export default Soma;
